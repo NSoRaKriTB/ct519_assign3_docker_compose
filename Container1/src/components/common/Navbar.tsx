bc8f0f54c504dde9d2017bec710885f1850e113a
NSoRaKriTB <Sorakrit.main@gmail.com>
2024-07-14 12:01:21 +0700

    Edit[all] Edit Docker-Compose to RP

--- Container1/src/components/common/Navbar.tsx
+++ Container1/src/components/common/Navbar.tsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 const Navbar: React.FC = () => {
   return (
     <div className='bg-blue-900 w-full h-16 flex justify-center items-center'>
       <div className='flex justify-between items-center w-[600px]'>
-        <Link to="/" className='text-white text-xl'>My Research</Link>
+        <a href="/" className='text-white text-xl'>My Research</a>
         {/* <Link to="/playlist" onClick={ } className='text-white text-xl'>Playlist</Link> */}
         {/*    <a href="/" onclick="javascript:event.target.port=8000" style="color: #fff; font-size: 20px;">My Research</a> */}
 
-        <Link to='/' onClick={() => window.location.port = "80"} className='text-white text-xl'>Playlist</Link>
+        <a href={`/about/`} className='text-white text-xl'>About</a>
       </div>
     </div>
   )
